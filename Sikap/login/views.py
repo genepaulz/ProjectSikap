8f38823e92c176257f487e456f7ce8345e04f882
genepaulz <zafra.genepaulm2013@gmail.com>
2021-04-07 22:13:55 +0800

    prep part2

--- Sikap/login/views.py
+++ Sikap/login/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views.generic import View,TemplateView
+from django.http import HttpResponse
+from .models import *
+#from .forms import *
 # Create your views here.
+class LoginView(View):
+    def get(self,request):
+        return render(request,'blogin.html')
+
+    # def post(self,request):        
+    #     user = request.POST.get("user")        
+    #     password = request.POST.get("pass")
+    #     q = Admin.objects.get(username = user)
+    #     if q.password == password:
+    #         return redirect('app:dashboard_view')
+    #     else:
+    #         return HttpResponse("User does not exist!")
