e6e80e22b11f598a5b04faa4cef746b366d8eb7f
genepaulz <zafra.genepaulm2013@gmail.com>
2021-04-07 22:05:28 +0800

    Prepped App for backend testing

---
+++ Sikap/login/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'login'
+# urlpatterns = [
+#     path('',views.LoginView.as_view(),name="login_view"),
+#     path('dashboard/',views.DashboardView.as_view(),name="dashboard_view"),
+#     path('products/',views.ProductView.as_view(),name="products_view"),
+#     path('regproducts/',views.RegisterProductView.as_view(),name="reg_products_view"),
+#     path('customers/',views.CustomerView.as_view(),name="customers_view"),
+#     path('regcustomers/',views.RegisterCustomerView.as_view(),name="reg_customers_view"),
+#     path('demo/',views.DemoView.as_view(),name="demo_view"),
+#     path('buy/',views.DemobView.as_view(),name="demob_view"),
+#     path('end/',views.EndView.as_view(),name="end_view"),
+# ]
